4acc985914d569b00e7380136f56777ea80dee72
Michel Figueira <michelfigueiratemp@gmail.com>
2021-12-17 20:12:24 -0300

    DataAnnotations

--- PokaList.Application/Dtos/GroupDto.cs
+++ PokaList.Application/Dtos/GroupDto.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PokaList.Application.Dtos
 {
     public class GroupDto
     {
         public int Id { get; set; }
+        [Required]
+        [MinLength(5), MaxLength(50)]
         public string Title { get; set; }
         public IEnumerable<PokaDto> Pokas { get; set; }
     }
--- PokaList.Application/Dtos/PokaDto.cs
+++ PokaList.Application/Dtos/PokaDto.cs
@@ -1,10 +1,15 @@
-﻿namespace PokaList.Application.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokaList.Application.Dtos
 {
     public class PokaDto
     {
         public int Id { get; set; }
+        [Required]
+        [MinLength(5), MaxLength(50)]
         public string Title { get; set; }
         public string Description { get; set; }
+        [Required]
         public int GroupId { get; set; }
         public GroupDto Group { get; set; }
     }
