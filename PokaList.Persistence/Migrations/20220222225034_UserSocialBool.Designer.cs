29c4f3b659ffb71ccba776cb8703201d06f478ec
Michel Figueira <michelfigueiratemp@gmail.com>
2022-03-01 09:36:28 -0300

    SocialConnect

--- PokaList.Persistence/Migrations/20220222225034_UserSocialBool.Designer.cs
+++ PokaList.Persistence/Migrations/20220222225034_UserSocialBool.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PokaList.Persistence.Contexts;
 
 namespace PokaList.Persistence.Migrations
 {
     [DbContext(typeof(PokaListContext))]
-    partial class PokaListContextModelSnapshot : ModelSnapshot
+    [Migration("20220222225034_UserSocialBool")]
+    partial class UserSocialBool
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PokaList.Persistence/Migrations/20220222225034_UserSocialBool.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PokaList.Persistence.Migrations
+{
+    public partial class UserSocialBool : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "socialLogin",
+                table: "AspNetUsers",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "socialLogin",
+                table: "AspNetUsers");
+        }
+    }
+}
